300e67af3f5f20e601f6bd26a6fc8f9efda293e9
Joe Gregorio <jcgregorio@google.com>
2010-09-14 15:17:28 -0400

    Change user-agent to match everyone else.

---
+++ runtests.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import glob
+import logging
+import os
+import sys
+import unittest
+
+from trace import fullmodname
+try:
+    from tests.utils import cleanup
+except:
+    def cleanup():
+        pass
+
+sys.path.insert(0, os.getcwd())
+
+verbosity = 1
+if "-q" in sys.argv or '--quiet' in sys.argv:
+    verbosity = 0
+if "-v" in sys.argv or '--verbose' in sys.argv:
+    verbosity = 2
+
+if verbosity == 0:
+  logging.disable(logging.CRITICAL)
+elif verbosity == 1:
+  logging.disable(logging.ERROR)
+elif verbosity == 2:
+  logging.basicConfig(level=logging.DEBUG)
+
+
+def build_suite(folder):
+  # find all of the test modules
+  modules = map(fullmodname, glob.glob(os.path.join(folder, 'test_*.py')))
+  print "Running the tests found in the following modules:"
+  print modules
+
+  # load all of the tests into a suite
+  try:
+      return unittest.TestLoader().loadTestsFromNames(modules)
+  except Exception, exception:
+      # attempt to produce a more specific message
+      for module in modules:
+          __import__(module)
+      raise
+
+unit_tests = build_suite('tests')
+functional_tests = build_suite('functional_tests')
+
+# run test suites
+unittest.TextTestRunner(verbosity=verbosity).run(unit_tests)
+unittest.TextTestRunner(verbosity=verbosity).run(functional_tests)
+cleanup()
---
+++ samples/cmdline/buzz.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python2.4
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 Google Inc. All Rights Reserved.
+
+"""Simple command-line example for Buzz.
+
+Command-line application that retrieves the users
+latest content and then adds a new entry.
+"""
+
+__author__ = 'jcgregorio@google.com (Joe Gregorio)'
+
+
+from apiclient.discovery import build
+
+import httplib2
+import pickle
+
+
+def main():
+  f = open("buzz.dat", "r")
+  credentials = pickle.loads(f.read())
+  f.close()
+
+  http = httplib2.Http()
+  http = credentials.authorize(http)
+
+  p = build("buzz", "v1", http=http)
+  activities = p.activities()
+  activitylist = activities.list(scope='@self', userId='@me')
+  print activitylist['items'][0]['title']
+  activities.insert(userId='@me', body={
+    'title': 'Testing insert',
+    'object': {
+      'content': u'Just a short note to show that insert is working. ☄',
+      'type': 'note'}
+    }
+  )
+
+if __name__ == '__main__':
+  main()
